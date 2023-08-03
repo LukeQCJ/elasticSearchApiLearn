c30f7df670b7f038c1fd1a9acef1db21f657a830
LukeQCJ <853223579@qq.com>
2023-08-03 16:04:03 +0800

    commit maven config

---
+++ src/main/org/luke/LukeApplication.java
@@ -0,0 +1,11 @@
+package org.luke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LukeApplication.class, args);
+    }
+}
